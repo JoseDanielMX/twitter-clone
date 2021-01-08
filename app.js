0abe9841acb2f6b3977527337f3a3b82280e5fa0
JoseDanielMX <jduvmx@gmail.com>
2021-01-08 16:43:06 -0600

    Created API endpoint /tweets to return a list of tweets based on query

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const port = 3000
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-    res.send("Hello world");
+app.get('/tweets', (req, res) => {
+    const query = req.query.q;
+    const max_results = req.query.max_results;
+    const url = "https://api.twitter.com/2/tweets/search/recent";
+    axios.get(url, {
+        params: {
+            query: query,
+            max_results: max_results
+        },
+        headers: {
+            "Authorization": `Bearer ${process.env.TWITTER_API_TOKEN}`
+        }
+    }).then((response) => {
+        res.status(200).send(response.data);
+    }).catch((error) => {
+        res.status(400).send(error);
+    })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
