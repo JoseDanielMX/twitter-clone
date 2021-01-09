1eb0b67435e337aeaeae7a87df895f92b05ea760
JoseDanielMX <jduvmx@gmail.com>
2021-01-08 19:33:30 -0600

    Created Twitter get() helper function to move Twitter API logic

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require('express')
 const app = express()
-const axios = require('axios')
+const Twitter = require('./api/helpers/twitter')
+const twitter = new Twitter()
 const port = 3000
 require('dotenv').config()
 
 app.get('/tweets', (req, res) => {
     const query = req.query.q;
     const max_results = req.query.max_results;
-    const url = "https://api.twitter.com/2/tweets/search/recent";
-    axios.get(url, {
-        params: {
-            query: query,
-            max_results: max_results
-        },
-        headers: {
-            "Authorization": `Bearer ${process.env.TWITTER_API_TOKEN}`
-        }
-    }).then((response) => {
+    twitter.get(query, max_results).then((response) => {
         res.status(200).send(response.data);
     }).catch((error) => {
         res.status(400).send(error);
